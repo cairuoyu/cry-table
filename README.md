7418a9a0d5c083658cf9c40a0642c04e9a662db5
fw\cry <>
2019-11-21 17:25:56 +0800

    修改readme

--- README.md
+++ README.md
@@ -1,2 +1,53 @@
 # cry-table
-一个基于vue的表格组件
+https://www.npmjs.com/package/cry-table
+---
+一个基于vue、element-ui的表格组件demo
+
+![image](./public/demo1.png)
+## 安装
+```bash
+npm i
+或者
+yarn
+```
+
+## 运行
+```bash
+npm run dev
+```
+
+## 访问
+http://localhost:9090/
+
+## 最简使用
+```html
+<template>
+  <cry-table :listConfig="listConfig" :listData="listData" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      listData: [],
+      listConfig: {
+        columns: [
+          { prop: 'country', label: '国家' },
+          { prop: 'province', label: '省份' },
+          { prop: 'city', label: '城市' }
+        ]
+      }
+    }
+  },
+  created() {
+    this.listData = [
+      {
+        country: '中国',
+        province: '广东省',
+        city: '广州市'
+      }
+    ]
+  }
+}
+</script>
+```
--- src/App.vue
+++ src/App.vue
@@ -1,114 +1,13 @@
 <template>
   <div>
-    <el-button @click="addRow">增加一行</el-button>
-    <el-button @click="hiddenKeyColumn" v-if="!listConfig.columns[2].hidden">隐藏第三列</el-button>
-    <el-button @click="showColumn" v-if="listConfig.columns[2].hidden">显示第三列</el-button>
-    <cry-table
-      :listConfig="listConfig"
-      :listData="listData"
-      :border="true"
-      @row-save="rowSave"
-      @testButtonMethod1="testButtonMethod1"
-      @changeProvince="changeProvince"
-    />
+    <table1 />
   </div>
 </template>
 
 <script>
-import pcDict from './data/pc-code.json'
-
+import table1 from './demo/table1'
 export default {
   name: 'app',
-  components: {
-  },
-  data() {
-    return {
-      pcDict,
-      cityDict: [],
-      options: { border: false },
-      listData: [],
-      listConfig: {
-        operateType: 1,
-        edit: false,
-        confirmBeforRemove: false,
-        index: true,
-        multi: true,
-        columns: [
-          { prop: 'country', label: '国家' },
-          {
-            prop: 'province',
-            label: '省份',
-            type: 'select',
-            options: pcDict,
-            valueKey: 'code',
-            labelKey: 'name',
-            changeMethod: 'changeProvince'
-          },
-          {
-            prop: 'city',
-            label: '城市',
-            type: 'select',
-            optionsKey: 'cityDict',
-            valueKey: 'code',
-            labelKey: 'name'
-          },
-          {
-            prop: 'year',
-            label: '年份',
-            type: 'year'
-          },
-          {
-            prop: 'month',
-            label: '月份',
-            type: 'month'
-          },
-          {
-            prop: 'population',
-            label: '人口',
-            type: 'input-number'
-          },
-          {
-            prop: 'memo',
-            label: '备注',
-            type: 'input'
-          },
-          {
-            label: '自定义按钮',
-            type: 'button',
-            width: '220px',
-            options: [
-              { buttonName: '按钮1', methodName: 'testButtonMethod1', hiddenKey: 'hk1' },
-              { buttonName: '按钮2', methodName: 'testButtonMethod2', hiddenKey: 'hk2', type: 'warning' },
-              { buttonName: '按钮3', type: 'text' }
-            ]
-          }
-        ]
-      }
-    }
-  },
-  created() {
-    this.listData = [{ country: '中国', edit: true }]
-  },
-  methods: {
-    async changeProvince(row, rowIndex) {
-      row.cityDict = this.pcDict.find(v => v.code === row.province).children
-      row.city = null
-    },
-    addRow() {
-      this.listData.push({ country: '中国', edit: true })
-    },
-    hiddenKeyColumn() {
-      this.$set(this.listConfig.columns[2], 'hidden', true)
-    },
-    showColumn() {
-      this.$set(this.listConfig.columns[2], 'hidden', false)
-    },
-    testButtonMethod1(row, rowIndex) {
-      row.hk2 = !row.hk2
-    },
-    rowSave(row, rowIndex) {
-      this.$message('保存成功')
-    }
-  }
+  components: { table1 }
 }
 </script>
